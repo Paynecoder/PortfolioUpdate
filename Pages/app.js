dd0dfbbb49b44d6828d5a6b08916c64f2c6a280b
paynecoder <joshpayne03@icloud.com>
2022-11-30 22:09:58 -0800

    First

---
+++ Pages/app.js
@@ -0,0 +1,47 @@
+
+/* 
+
+music player might be in site, might not
+
+function playaudio() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+*/
+
+function master() {
+    changeimage();
+    changeskill();
+    changetitle();
+    changetext();
+}
+
+function changeskill() {
+  var skill = document.getElementById('welearnskill');
+  if (skill.src.match("/Assets_Skills/FIGMA.svg")) {
+    skill.src = "/Assets_Skills/PS.svg"
+  } 
+}
+
+function changeimage() {
+  var image = document.getElementById('welearn');
+  if (image.src.match("/Assets/weLearn.jpeg")) {
+    image.src = "/Assets/PS-Midterm.png";
+  } 
+}
+
+function changetitle() {
+  var title = document.getElementById('welearntitle')
+  if (title.textContent.match("weLearn")) {
+    title.textContent = "PS5 Game Cover"
+  }
+}
+
+function changetext() {
+  var text = document.getElementById('welearntext')
+  if (text.textContent.match("A UX Figma design project, weLearn is a mobile studying app created by myself & Carter V.")) {
+    text.textContent = "A mockup Playstation 5 video game cover of a basketball game."
+  }
+}
+
+
