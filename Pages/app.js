0f234b0bc3487772633cba5f5523eaa30058a061
Joshua Payne <jpayne@Joshuas-MacBook-Air.local>
2022-12-01 11:59:26 -0800

    mobileviewadded

--- Pages/app.js
+++ Pages/app.js
@@ -1,4 +1,3 @@
-
 /* 
 
 music player might be in site, might not
@@ -10,46 +9,50 @@ function playaudio() {
 */
 
 function master() {
-    changeimage();
-    changeskill();
-    changetitle();
-    changetext();
+  changeimage();
+  changeskill();
+  changetitle();
+  changetext();
 }
 
 function changeskill() {
-  var skill = document.getElementById('welearnskill');
+  var skill = document.getElementById("welearnskill");
   if (skill.src.match("/Assets_Skills/FIGMA.svg")) {
-    skill.src = "/Assets_Skills/PS.svg"
+    skill.src = "/Assets_Skills/PS.svg";
   } else {
-    skill.src = "/Assets_Skills/FIGMA.svg"
+    skill.src = "/Assets_Skills/FIGMA.svg";
   }
 }
 
 function changeimage() {
-  var image = document.getElementById('welearn');
+  var image = document.getElementById("welearn");
   if (image.src.match("/Assets/weLearn.jpeg")) {
     image.src = "/Assets/PS-Midterm.png";
   } else {
-    image.src = "/Assets/weLearn.jpeg"
+    image.src = "/Assets/weLearn.jpeg";
   }
 }
 
 function changetitle() {
-  var title = document.getElementById('welearntitle')
+  var title = document.getElementById("welearntitle");
   if (title.textContent.match("weLearn")) {
-    title.textContent = "PS5 Game Cover"
+    title.textContent = "PS5 Game Cover";
   } else {
-    title.textContent = "weLearn"
+    title.textContent = "weLearn";
   }
 }
 
 function changetext() {
-  var text = document.getElementById('welearntext')
-  if (text.textContent.match("A UX Figma design project, weLearn is a mobile studying app created by myself & Carter V.")) {
-    text.textContent = "A mockup Playstation 5 video game cover of a basketball game."
+  var text = document.getElementById("welearntext");
+  if (
+    text.textContent.match(
+      "A UX Figma design project, weLearn is a mobile studying app created by myself & Carter V."
+    )
+  ) {
+    text.textContent =
+      "A mockup Playstation 5 video game cover of a basketball game.";
   } else {
-    text.textContent = "A UX Figma design project, weLearn is a mobile studying app created by myself & Carter V."
+    text.textContent =
+      "A UX Figma design project, weLearn is a mobile studying app created by myself & Carter V.";
   }
-} 
-
-
+}
